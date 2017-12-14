9940bb989fbd4b589430a50d6b5c13f050b8916e
Marivaldo Sena <marivaldo.sena@live.com>
2017-12-13 22:20:44 -0200

    Add show and index pic pages

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,12 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
+
   def index
-    # index
+    @pics = Pic.all.order('created_at DESC')
+  end
+
+  def show
+    find_pic
   end
 
   def new
@@ -9,9 +15,22 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: 'Yesss! It was posted!'
+    else
+      render 'new'
+    end
   end
 
   private
+  def find_pic
+    begin
+      @pic = Pic.find(params[:id])
+    rescue
+        redirect_to root_path, notice: "There's no pics in the typed in location!"
+    end
+  end
 
   def pic_params
     params.require(:pic).permit(:title, :description)
